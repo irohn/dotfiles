734d3f3e61d02b54970daf69282d314cf4766e6a
= <orisneh@gmail.com>
2024-02-25 02:00:58 +0200

    Added fzf source and eza aliases

--- .config/alias.d/alternatives.sh
+++ .config/alias.d/alternatives.sh
@@ -11,5 +11,8 @@ command -v bat > /dev/null 2>&1 && \
 
 # [[ eza - a modern ls alternative ]]
 command -v eza > /dev/null 2>&1 && \
-  alias ls='eza'
+  alias ls='eza' && \
+  alias ll='eza -lAh' && \
+  alias la='eza -laa' && \
+  alias l='eza -l'
 
