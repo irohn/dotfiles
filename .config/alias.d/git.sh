9e3ed24c219ebb2bab430a039e1d882d48f3b008
= <orisneh@gmail.com>
2024-02-25 12:03:49 +0200

    Added green-git (ggit) alias to use green key

--- .config/alias.d/git.sh
+++ .config/alias.d/git.sh
@@ -1,4 +1,6 @@
 
+alias ggit='git -c "core.sshCommand=ssh -i ~/.ssh/green_id_rsa -F /dev/null"'
+
 # [E]dit [G]it [S]tatus
 egs() {
   ${EDITOR:-vi} $(git status --porcelain | fzf --preview-window right --preview 'bat --plain --color=always {2}' | sed s/^...//)
