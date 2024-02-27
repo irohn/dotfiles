54202d3a75754880c672f07f043cce1db0d1bfed
= <orisneh@gmail.com>
2024-02-27 19:54:02 +0200

    Added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- Color scheme
+config.color_scheme = "Tokyo Night"
+
+-- Window customizations
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Font settings
+config.font = wezterm.font_with_fallback({
+	"CaskaydiaCove Nerd Font",
+	"Nerd Font Symbols",
+	"Noto Color Emoji",
+	"JetBrains Mono",
+})
+config.font_size = 16
+
+return config
